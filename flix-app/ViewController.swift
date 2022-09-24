58c00f751c4b915d59e17890c1f253718231789b
Kent Canonigo <kcanonigo@middlebury.edu>
2022-09-23 21:26:08 -0400

    Initial Commit

---
+++ flix-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flix-app
+//
+//  Created by Kent Brylle Canonigo on 9/23/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
